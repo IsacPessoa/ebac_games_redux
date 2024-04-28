20ed29290f8fca1e4232cd1d2da538edef72a460
Isac Pessoa <isacdavidpessoa@hotmail.com>
2024-04-28 15:34:21 -0300

    commitado

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,18 +1,26 @@
-import { configureStore } from '@reduxjs/toolkit'
+import {
+  combineReducers,
+  configureStore,
+  PreloadedState
+} from '@reduxjs/toolkit'
 
 import carrinhoReducer from './reducers/carrinho'
 
 import api from '../services/api'
-import { getDecorators } from 'typescript'
-import { buildGetDefaultMiddleware } from '@reduxjs/toolkit/dist/getDefaultMiddleware'
 
-export const store = configureStore({
-  reducer: {
-    carrinho: carrinhoReducer,
-    [api.reducerPath]: api.reducer
-  },
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(api.middleware)
+const rootReducer = combineReducers({
+  carrinho: carrinhoReducer,
+  [api.reducerPath]: api.reducer
 })
 
-export type RootReducer = ReturnType<typeof store.getState>
+export function configuraStore(preloadedState?: PreloadedState<RootState>) {
+  return configureStore({
+    reducer: rootReducer,
+    middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware().concat(api.middleware),
+    preloadedState
+  })
+}
+
+export type RootState = ReturnType<typeof rootReducer>
+export type AppStore = ReturnType<typeof configuraStore>
